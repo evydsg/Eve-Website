af6f6bbd5a52b182cfdbea985c728df10f65ac71
Evelise Karen Guenda <evelisekguenda@gmail.com>
2026-03-13 08:27:37 -0700

    Updated Profile Picture

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About(){
             <div className="about-container">
 
                 <div className="about-left">
-                    <img src ="/eve.jpeg" alt="Eve Guenda" className="about-photo"/>
+                    <img src ="/eve-about.jpeg" alt="Eve Guenda" className="about-photo"/>
                 </div>
 
                 <div className="about-right">
@@ -13,8 +13,10 @@ function About(){
                     <br/>behind it all.</h2>
                     <div className = "about-divider"></div>
                     <p className="about-bio">
-                    Everything I do is mainly rooted in God and guided by faith. I’m Evelise, though most people call me Eve, a software engineer, builder, and curious mind originally from Mozambique, now living in the United States. My life has been a series of leaps into the unknown: new countries, new careers, new challenges. I didn’t grow up around technology, but I fell in love with it because I realized code is more than logic. It is a tool to solve real human problems. I am driven by purpose more than titles. I care about impact, about creating things that actually help people, and about becoming someone younger me would have prayed to become.
-My journey hasn not been linear. I have faced closed doors, sudden changes, and setbacks that felt like endings, but they always turned into redirections. I build software, but I’m also building a company, a community, and a life aligned with meaning. I love learning, creating, praying, gaming, and documenting the process of becoming. What motivates me is simple: growth, service, and hope. I want to create things that make people feel less alone, more capable, and a little more optimistic about their future. Through every step, success or uncertainty, I choose to keep moving forward and to Stay Positive.
+                    Everything I do is mainly rooted in God and guided by faith. 
+                    I’m Evelise, though most people call me Eve, a software engineer, builder, and curious mind originally from Mozambique, now living in the United States. My life has been a series of leaps into the unknown: new countries, new careers, new challenges. I didn’t grow up around technology, but I fell in love with it because I realized code is more than logic. It is a tool to solve real human problems. I am driven by purpose more than titles. I care about impact, about creating things that actually help people, and about becoming someone younger me would have prayed to become.
+                    My journey has not been linear. I
+ have faced closed doors, sudden changes, and setbacks that felt like endings, but they always turned into redirections. I build software, but I’m also building a company, a community, and a life aligned with meaning. I love learning, creating, praying, gaming, and documenting the process of becoming. What motivates me is simple: growth, service, and hope. I want to create things that make people feel less alone, more capable, and a little more optimistic about their future. Through every step, success or uncertainty, I choose to keep moving forward and to Stay Positive.
                     </p>
                     <div className="about-tags">
                         <span className="tag">Creative</span>
